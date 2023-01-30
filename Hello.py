3c042b99766d5a1b255afa43691af351583f53d4
Daniil924785 <54883329+Daniil924785@users.noreply.github.com>
2023-01-30 10:33:24 +0300

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,16 @@
+print('Hello World')
+print('hello world!')
+a=input('Введите команду(+;-;/;*;А;В)')
+if a=='A':
+	print('A')
+if a=='A':
+	print('B')
+if a=='+':
+	print(2+2)
+if a=='-':
+	print(2-2)
+if a=='*':	
+	print(2*2)
+if a=='/':
+	print(2/2)
+input('Конец')
